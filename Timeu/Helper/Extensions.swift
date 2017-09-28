ab64866c74f50d815a15384352dae1afa9779b52
Sebastian Limbach <bastilimbach@gmail.com>
2017-09-28 22:47:22 +0200

    Add extensions helper

---
+++ Timeu/Helper/Extensions.swift
@@ -0,0 +1,15 @@
+//
+//  Extensions.swift
+//  Timeu
+//
+//  Created by Sebastian Limbach on 28.09.2017.
+//  Copyright © 2017 Sebastian Limbach. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+    open class var timeuGray: UIColor {
+        return UIColor.init(red: 244/255, green: 244/255, blue: 247/255, alpha: 1)
+    }
+}
